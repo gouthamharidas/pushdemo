6ce0378ddfa94185134b67fc0d9418b16e38e323
gouthamharidas <117807315+gouthamharidas@users.noreply.github.com>
2023-05-30 17:48:06 +0530

    Update Program.cs "bugs"

--- PushListenerForLinux/Program.cs
+++ PushListenerForLinux/Program.cs
@@ -1,11 +1,40 @@
 using PushListenerForLinux;
 
-IHost host = Host.CreateDefaultBuilder(args)
-    .UseSystemd()
-    .ConfigureServices(services =>
-    {
-        services.AddHostedService<Worker>();
-    })
-    .Build();
+public interface IWorker
+{
+    void DoWork();
+}
 
-await host.RunAsync();
+public class NullWorker : IWorker
+{
+    public void DoWork()
+    {
+        // Null implementation intentionally left blank
+    }
+}
+
+public class Worker : IWorker
+{
+    public void DoWork()
+    {
+        // Original implementation of the DoWork method
+        // Add your bug-inducing logic here
+    }
+}
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        IHost host = Host.CreateDefaultBuilder(args)
+            .UseSystemd()
+            .ConfigureServices(services =>
+            {
+                // Register the Worker class as the service implementation
+                services.AddSingleton<IWorker, Worker>();
+            })
+            .Build();
+
+        await host.RunAsync();
+    }
+}
